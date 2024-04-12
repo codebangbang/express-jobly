9487a6a0c3aa6205f52b8d7184f1b7d2bdda6e5e
ryanfair <ryfair@gmail.com>
2024-04-12 18:46:29 -0400

    Part 1 Complete

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,6 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+// This function is used to convert the data to be updated into a SQL string. It takes in two arguments: dataToUpdate and jsToSql. dataToUpdate is an object with the data to be updated. jsToSql is an object that maps the keys in dataToUpdate to the column names in the database. The function returns an object with two keys: setCols and values. setCols is a string that contains the column names and values to be updated. values is an array of the values to be updated. The function throws an error if there is no data to update.
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
